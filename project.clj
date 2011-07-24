b94079f0ecac0a8cff6131c373d810fdc5484e87
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-07-24 10:24:57 +0200

    Moved Clojure sources to src/clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject ws-cljs "0.1-SNAPSHOT"
   :description "WebSocket and ClojureScript gitorial"
   :main "ws-cljs.core"
+  :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [aleph "0.2.0-alpha2"]
                  [compojure "0.6.5"]
