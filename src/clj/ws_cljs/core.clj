d3ac4f8d685ff3aa1ad24a6c20afcb0d037770c8
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-08-02 15:51:50 +0200

    Simplifying handling messages on server side.

--- src/clj/ws_cljs/core.clj
+++ src/clj/ws_cljs/core.clj
@@ -26,44 +26,56 @@
   (fn []
     (println ip ": Drained" @nick)))
 
+(defn receive-handler
+  "Receive message over WebWocket event handler.
+  Arguments:
+  - ip - ip of client
+  - ch - channel to be used
+  - nickname - atom to push nickname once set
+  - msg - first message received over channel"
+  [ip ch nickname msg]
+  (when msg ;; last message in a channel is nil, so it can be the only one as well.
+    (let [[_ nick] (re-matches #"\/nick (.+)" msg)] 
+      (if nick
+        (do (println ip ": /nick" nick)
+            (reset! nickname nick)
+            (lamina/enqueue broadcast-channel (str "/joined " nick)))
+        (lamina/enqueue ch "/identify"))
+      
+      (lamina/siphon
+       (lamina/map*
+        (fn [msg]
+          (when msg
+            (if (not= :no-name @nickname)
+              (let [[_ cmd body] (re-matches #"\/([^ ]+) (.*)" msg)]
+                (println ip ":" nick ":" cmd ":" body)
+                (cond
+                 (= cmd "msg") (str "/msg " nick " " body)
+                 (= cmd "nick") (let [old-nick @nickname]
+                                  (reset! nickname body)
+                                  (str "/nick " old-nick " " body))
+                 :default (lamina/enqueue ch (str "/error Command /" cmd " not supported"))))
+              (lamina/enqueue ch "/identify"))))
+        ch)
+       broadcast-channel)
+      (lamina/siphon broadcast-channel ch)
+      (lamina/enqueue ch (format "/motd Hello %s! Enjoy your stay and share if you like it." nick)))))
+
 (defn chat-handler [ch {ip :remote-addr}]
   (let [nickname (atom :no-name)]
     (println ip ": Connected")
     (swap! counter inc)
-    (lamina/enqueue broadcast-channel (str "/count " @counter))
-
     ;; register close handler
     (lamina/on-closed ch (create-close-handler ip nickname))
 
-    ;; handler
+    ;; drain handler
     (lamina/on-drained ch (create-drained-handler ip nickname))
 
-    (lamina/receive ch
-                    (fn [name]
-                      (when name ;; last message in a channel is nil,
-                        ;; so it can be the only one as well.
-                        (println ip "->" name)
-                        (let [[_ nick] (re-matches #"\/nick (.+)" name)] 
-                          (println ip ": /nick" nick)
-                          (reset! nickname nick)
-                          (lamina/enqueue broadcast-channel (str "/joined " nick))
-                          (lamina/siphon
-                           (lamina/map*
-                            (fn [msg]
-                              (when msg
-                                (let [[_ cmd body] (re-matches #"\/([^ ]+) (.*)" msg)]
-                                  (println ip ":" nick ":" cmd ":" body)
-                                  (cond
-                                   (= cmd "msg") (str "/msg " nick " " body)
-                                   (= cmd "nick") (let [old-nick @nickname]
-                                                    (reset! nickname body)
-                                                    (str "/nick " old-nick " " body))
-                                   :default (lamina/enqueue ch (str "/error Command /" cmd " not supported"))))))
-                            ch)
-                           broadcast-channel)
-                          (lamina/siphon broadcast-channel ch)
-                          (lamina/enqueue ch (format "/motd Hello %s! Enjoy your stay and share if you like it." nick))))))
-    (lamina/enqueue ch "/msg root Register with /nick <your nick name>")))
+    ;; message handler
+    (lamina/receive ch #(receive-handler ip ch nickname %))
+
+    ;; say hi
+    (lamina/enqueue broadcast-channel (str "/count " @counter))))
 
 (compojure/defroutes my-app 
   (compojure/GET "/" [] (redirect "index.html"))
