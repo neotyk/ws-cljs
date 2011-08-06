298caf4b826cd7628eed7b5dca2404545d2e043c
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-08-06 15:57:25 +0200

    swank is needed for normal mode.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [aleph "0.2.0-alpha2"]
-                 [compojure "0.6.5"]]
-  :dev-dependencies [[slamhound "1.2.0"]
-                     [swank-clojure "1.3.2"]])
+                 [compojure "0.6.5"]
+                 [swank-clojure "1.3.2"]]
+  :dev-dependencies [[slamhound "1.2.0"]])
