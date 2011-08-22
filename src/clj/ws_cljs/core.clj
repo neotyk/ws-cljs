d9cd5f600fcabf6b0746915634299e987e57a66f
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-08-22 11:44:38 +0200

    Pipelines first cut.

--- src/clj/ws_cljs/core.clj
+++ src/clj/ws_cljs/core.clj
@@ -3,6 +3,7 @@
         [ring.middleware.file-info :only [wrap-file-info]]
         [ring.util.response :only [redirect]])
   (:require [compojure.route :as route]
+            [clojure.contrib.logging :as log]
             [aleph.http :as aleph]
             [lamina.core :as lamina]
             [compojure.core :as compojure]
@@ -10,24 +11,26 @@
 
 (def broadcast-channel (lamina/permanent-channel))
 
-(def *USERS* (atom #{}))
+(def *USERS* (ref #{}))
 
 (defn state-add-user!
   [nick]
-  (swap! *USERS* conj nick))
+  (dosync (commute *USERS* conj nick))
+  nick)
 
 (defn state-rm-user!
   "Removes user from list of registered users."
   [nick]
-  (swap! *USERS* disj nick))
+  (dosync (commute *USERS* disj nick)))
 
 (defn state-mv-user!
   "Renames user.
-TODO: Check if to user was not registered already."
+Has to be executed in tranasation."
   [from to]
-  (swap! *USERS* #(-> %
-                      (disj from)
-                      (conj to))))
+  (commute *USERS*
+           #(-> %
+                (disj from)
+                (conj to))))
 
 (defn state-users-count
   []
@@ -42,7 +45,7 @@ TODO: Check if to user was not registered already."
   "Create WebSocket close handler."
   [ip nick]
   (fn []
-    (println ip ": Closing" @nick)
+    (log/info (str ip " : Closing " @nick))
     (state-rm-user! @nick)
     (lamina/enqueue broadcast-channel (str "/left " @nick))))
 
@@ -50,7 +53,7 @@ TODO: Check if to user was not registered already."
   "Create WebSocker backing channel drained halnder."
   [ip nick]
   (fn []
-    (println ip ": Drained" @nick)))
+    (log/info (str ip " : Drained " @nick))))
 
 (defn onMsg
   "On message handler.
@@ -65,34 +68,34 @@ TODO: Check if to user was not registered already."
            (contains? @*USERS* new-nick)
            (.contains new-nick " "))))
 
-(defn onNick
-  "On nick handler.
-  Called when nick command has been recived.
-  Changes nickname (local to channel) and global user list.
-  Notifies clients of nickname change.
-  If nick to change to was taken already, rejection is sent to client
-  in form of one argument nick command with original nick.
+(defn nick-pipe
+  "Pipeline executed on nick command"
+  []
+  (log/info "Creating nick pipeline")
+  (lamina/pipeline
+   (fn [{ch :channel nick :nick new-nick :body :as m}]
+     (log/info (str "Executing nick pipeline with: " m))
+     (let [old-nick @nick]
+       (if (valid-nick-change? new-nick)
+         (do ;; allowed to change
+           (log/info (format "nick '%s' -> '%s'"  old-nick new-nick))
+           (dosync
+            (state-mv-user! old-nick new-nick)
+            (ref-set nick new-nick))
+           (lamina/enqueue broadcast-channel (str "/nick " old-nick " " new-nick)))
+         (do ;; nickname already taken, or otherwise invalid
+           (log/info (format "nick '%s' !> '%s'"  old-nick new-nick))
+           (lamina/enqueue ch (str "/nick " old-nick))))))))
 
-  Arguments:
-  - ch - clients channel
-  - nick-a - nickname atom used by this connection
-  - new-nick - new nickname to change to if possible"
-  [ch nick-a new-nick]
-  (let [old-nick @nick-a]
-    (if (valid-nick-change? new-nick)
-      (do ;; allowed to change
-        (println (format "nick '%s' -> '%s'"  old-nick new-nick))
-        (state-mv-user! old-nick new-nick)
-        (reset! nick-a new-nick)
-        (lamina/enqueue broadcast-channel (str "/nick " old-nick " " new-nick)))
-      (do ;; nickname already taken
-        (println (format "nick '%s' !> '%s'"  old-nick new-nick))
-        (lamina/enqueue ch (str "/nick " old-nick))))))
-
-(defn sendList
-  "Lists all connected users"
-  [ch]
-  (lamina/enqueue ch (str "/users " (state-ls-users))))
+(defn send-users-list-pipe
+  "Pipeline to send current users list"
+  []
+  (let [users (state-ls-users)]
+    (lamina/pipeline
+     #(when-not (or (lamina/closed? %)
+                    (lamina/drained? %))
+        (log/info "listing users")
+        (lamina/enqueue % (str "/users " users))))))
 
 (defn receive-handler
   "Receive message over WebWocket event handler.
@@ -106,39 +109,57 @@ TODO: Check if to user was not registered already."
     (let [[_ cmd body] (re-matches #"\/([^ ]+) (.*)" msg)]
       (if cmd
         (do
-          (println ip ":" @nickname ":" cmd ":" body)
+          (log/info (str ip " : " @nickname " : " cmd " : " body))
           (cond
            (= cmd "msg") (onMsg ch nickname body)
-           (= cmd "nick") (onNick ch nickname body)
-           (= cmd "list") (sendList ch)
+           (= cmd "nick") ((nick-pipe) {:channel ch
+                                        :nick nickname
+                                        :body body})
+           (= cmd "list") ((send-users-list-pipe) ch)
            :default (lamina/enqueue ch (str "/error Command /" cmd " not supported"))))
         (do
-          (println ip ":" @nickname ": no command :" msg)
+          (log/info (str ip " : " @nickname " : no command : " msg))
           (lamina/enqueue broadcast-channel (str "/msg " @nickname " " msg)))))))
+
+(defn register-lifecycle-hooks
+  "Register lifecycle hooks for channel"
+  [ip nickname ch]
+  ((lamina/pipeline
+    #(create-close-handler ip %)
+    #(lamina/on-closed ch %))
+   nickname)
+  ((lamina/pipeline
+    #(create-drained-handler ip %)
+    #(lamina/on-drained ch %))
+   nickname))
+
+(defn register-new-user
+  "Registers new user"
+  [ip nickname]
+  ((lamina/pipeline
+    #(state-add-user! %)
+    #(lamina/enqueue broadcast-channel (str "/joined " %)))
+   @nickname))
 
 (defn connected-hook
   "Hook executed when new client connected."
   [ch ip nickname]
-  (println ip ": Connected, assigned nick:" @nickname)
-  (state-add-user! @nickname)
-  (lamina/enqueue broadcast-channel (str "/joined " @nickname))
-  
-  ;; register close handler
-  (lamina/on-closed ch (create-close-handler ip nickname))
+  (let [connect-pipeline (lamina/pipeline
+                          #(register-lifecycle-hooks ip nickname %)
+                          #(register-new-user ip nickname))]
+    (connect-pipeline ch)
 
-  ;; drain handler
-  (lamina/on-drained ch (create-drained-handler ip nickname))
+    ;; message handler
+    (lamina/receive-all ch #(receive-handler ip ch nickname %))
 
-  ;; message handler
-  (lamina/receive-all ch #(receive-handler ip ch nickname %))
+    ;; publish events from broadcast-channel to clients channel
+    (lamina/siphon broadcast-channel ch)
 
-  ;; publish events from broadcast-channel to clients channel
-  (lamina/siphon broadcast-channel ch)
+    ;; your nickname
+    (lamina/enqueue ch (str "/nick " @nickname))
 
-  ;; your nickname
-  (lamina/enqueue ch (str "/nick " @nickname))
-  ;; say hi
-  (sendList ch))
+    ;; say hi
+    ((send-users-list-pipe) ch)))
 
 (defn websocket-handler
   "All WebSocket connections start here.
@@ -147,7 +168,7 @@ Sets up local atom for nickname (closed over by all event handlers).
 Registers event handlers for new WebSocket connection (ch)."
   [ch {ip :remote-addr}]
   ;; Create random nickname for yet not identified users.
-  (connected-hook ch ip (atom (gensym "Guest_"))))
+  (connected-hook ch ip (ref (gensym "Guest_"))))
 
 (compojure/defroutes my-app 
   (compojure/GET "/" [] (redirect "index.html"))
@@ -163,5 +184,5 @@ Registers event handlers for new WebSocket connection (ch)."
                                  wrap-file-info
                                  aleph/wrap-ring-handler) {:port port :websocket true})
     (swank/start-repl 4005)
-    (println "Server started on port" port)))
+    (log/info (str "Server started on port " port))))
         
