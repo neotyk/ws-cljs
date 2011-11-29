dca7b27a754227b29ea0c2ca9d947aeab4f4a136
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-11-29 13:24:28 +0100

    Bump aleph version.
    This transitively brings new Netty with recent websocket spec.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :main "ws-cljs.core"
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [aleph "0.2.0-alpha2"]
+                 [aleph "0.2.0-rc2"]
                  [compojure "0.6.5"]
                  [swank-clojure "1.3.2"]]
   :dev-dependencies [[slamhound "1.2.0"]])
